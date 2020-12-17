ba8e580270d5c7d209eb741f8a39fd6fed855a3c
Naman Kumar Jha <naman.jha.10@gmail.com>
2020-12-17 09:23:38 +0530

    Back button ready

---
+++ app/src/main/java/com/example/currentlocation/ComeBack.java
@@ -0,0 +1,34 @@
+package com.example.currentlocation;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class ComeBack extends AppCompatActivity
+{
+
+    public static Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.come_activity);
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                this.finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
