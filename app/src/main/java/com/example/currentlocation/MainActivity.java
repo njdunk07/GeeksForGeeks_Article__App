e262f2e7a3bb7bfc65985a75ad094bcbfd13e2f8
Naman Kumar Jha <naman.jha.10@gmail.com>
2020-12-17 09:00:52 +0530

    Back button ready

--- app/src/main/java/com/example/currentlocation/MainActivity.java
+++ app/src/main/java/com/example/currentlocation/MainActivity.java
@@ -1,30 +1,30 @@
 package com.example.currentlocation;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.graphics.Typeface;
+import android.app.ActionBar;
+import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
 {
+
+    public static Button button;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Toolbar toolbar =  findViewById(R.id.toolbar);
-
-        // Custom title
-        TextView textCustomTitle = (TextView) findViewById(R.id.custom_title);
-
-        // Custom font
-        Typeface customFont = Typeface.createFromAsset(this.getAssets(), "fonts/Pacifico.ttf");
-
-        // Set
-        textCustomTitle.setTypeface(customFont);
-
-        setSupportActionBar(toolbar);
+        button = (Button) findViewById(R.id.butVar1);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), ComeBack.class);
+                startActivity(intent);
+            }
+        });
     }
 }
