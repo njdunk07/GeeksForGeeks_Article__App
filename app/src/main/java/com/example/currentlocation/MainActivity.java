becb7972ae872ac1b070892eb202139c25b1da24
Naman Kumar Jha <naman.jha.10@gmail.com>
2020-12-27 10:58:35 +0530

    Dice Game

--- app/src/main/java/com/example/currentlocation/MainActivity.java
+++ app/src/main/java/com/example/currentlocation/MainActivity.java
@@ -1,29 +1,47 @@
 package com.example.currentlocation;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.app.ActionBar;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity
 {
-
     public static Button button;
-    @Override
+    public static TextView textView;
+    public static ImageView img1, img2;     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button = (Button) findViewById(R.id.butVar1);
+        final int dice[] = {R.drawable.dice1, R.drawable.dice2, R.drawable.dice3, R.drawable.dice4, R.drawable.dice5, R.drawable.dice6};
+        button = findViewById(R.id.btVar1);
+        textView = findViewById(R.id.tvVar1);
+        img1 = findViewById(R.id.ivVar1);
+        img2 = findViewById(R.id.ivVar2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), ComeBack.class);
-                startActivity(intent);
+                Random random = new Random();
+                int num1 = random.nextInt(6);
+                Random random1 = new Random();
+                int num2 = random.nextInt(6);
+                if(num1>num2)
+                {
+                    textView.setText("WINNER : Player 1");
+                }
+                else if(num2>num1)
+                {
+                    textView.setText("WINNER : Player 2");
+                }
+                else
+                {
+                    textView.setText("RESULT : Draw");
+                }
+                img1.setImageResource(dice[num1]);
+                img2.setImageResource(dice[num2]);
             }
         });
     }
